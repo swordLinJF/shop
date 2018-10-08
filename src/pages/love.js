474fdefa504acc77cd72286658b01fda01f076d1
剑剑 <sword@jianjiandeMacBook-Pro.local>
2018-10-08 21:36:02 +0800

    新增一个人love页

--- src/pages/love.js
+++ src/pages/love.js
@@ -1,31 +1,11 @@
 import React from 'react'
 import svg from '../components/svg.css'
-// const IndexPage = () => (
-  
 
-//     <div>
-//       <div id="content">
-        
-//       </div>
-//       <div>
-//         <svg className="heart-loader"   xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 90 90" version="1.1">
-//           <g className="heart-loader__group">
-//             <path className="heart-loader__square" strokeWidth="1" fill="none" d="M0,30 0,90 60,90 60,30z"/>
-//             <path className="heart-loader__circle m--left" strokeWidth="1" fill="none" d="M60,60 a30,30 0 0,1 -60,0 a30,30 0 0,1 60,0"/>
-//             <path className="heart-loader__circle m--right" strokeWidth="1" fill="none" d="M60,60 a30,30 0 0,1 -60,0 a30,30 0 0,1 60,0"/>
-//             <path className="heart-loader__heartPath" strokeWidth="2" d="M60,30 a30,30 0 0,1 0,60 L0,90 0,30 a30,30 0 0,1 60,0" />
-//           </g>
-//         </svg>
-//       </div>
-//     </div>  
-// )
-// export default IndexPage
-
-class love extends React.Component{
+class Love extends React.Component{
   constructor(props){
     super(props)
     this.state = {
-      content: "呐，给你一颗小心心",
+      content: "呐，给你一颗小❤️❤️",
       love: "",
     }
     
@@ -76,4 +56,4 @@ class love extends React.Component{
   }
 }
 
-export default love
+export default Love
